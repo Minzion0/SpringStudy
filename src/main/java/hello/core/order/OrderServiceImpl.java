9785267948e5dcd09beff478aad17c0b9fc84aae
이민용 <aert12435@gmail.com>
2023-06-08 00:35:06 +0900

    Bean검색

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,13 +4,15 @@ import hello.core.discount.DiscountPolicy;
 
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRe;
     private final DiscountPolicy discount;
-
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRe = memberRepository;
         this.discount = discountPolicy;
