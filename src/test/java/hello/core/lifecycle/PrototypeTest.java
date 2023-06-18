7ba7857bbbd6658562d9c02429bd67586612cea1
이민용 <aert12435@gmail.com>
2023-06-19 02:27:46 +0900

    Bean검색

---
+++ src/test/java/hello/core/lifecycle/PrototypeTest.java
@@ -0,0 +1,39 @@
+package hello.core.lifecycle;
+
+import hello.core.scope.SingletonTest;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+       PrototypeBean singletonBean1 = ac.getBean(PrototypeBean.class);
+       PrototypeBean singletonBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        Assertions.assertThat(singletonBean1).isNotSameAs(singletonBean2);
+
+        singletonBean1.destroy();
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
